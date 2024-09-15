b5d65a79bd46f2af58ff2fe4fd61a96d4510904c
Mark Smith <aardmark.smith@gmail.com>
2024-09-15 21:18:46 +1000

    added python and csharp lsp servers

--- lua/aardmark/plugins/lsp-config.lua
+++ lua/aardmark/plugins/lsp-config.lua
@@ -155,8 +155,9 @@ return {
 			zls = {},
 			clangd = {},
 			gopls = {},
-			-- pyright = {},
+			pyright = {},
 			rust_analyzer = {},
+			omnisharp = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:

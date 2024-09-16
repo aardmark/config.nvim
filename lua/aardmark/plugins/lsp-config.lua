fff37c515999c9662f08a7c2255360530052fff6
Mark Smith <aardmark.smith@gmail.com>
2024-09-16 13:33:00 +1000

    added some more lsp servers

--- lua/aardmark/plugins/lsp-config.lua
+++ lua/aardmark/plugins/lsp-config.lua
@@ -158,6 +158,10 @@ return {
 			pyright = {},
 			rust_analyzer = {},
 			omnisharp = {},
+			ts_ls = {},
+			html = {},
+			cssls = {},
+			emmet_ls = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
